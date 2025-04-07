89019be0f85f8b326700d455eabb49114dd99568
MOSESOROPEEZA21 <mooropeza@s.sfusd.edu>
2025-04-07 14:20:34 -0700

    Collectible  and level completed

--- _heart.gd
+++ _heart.gd
@@ -5,4 +5,4 @@ func _on_body_entered(body: Node2D) -> void:
 	queue_free()
 	var hearts = get_tree().get_nodes_in_group("Hearts")
 	if hearts.size() == 1:
-		print("Level Completed")
+		Events.level_completed.emit()
