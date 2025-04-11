7a5484f31d06b546caaab718e7670eedd1e4da74
AnOropeza26 <159198099+AnOropeza26@users.noreply.github.com>
2025-04-11 13:42:25 -0700

    Dialouge start menu

---
+++ dialouge.gd
@@ -0,0 +1,31 @@
+extends Control
+
+@onready var rich_text_label: RichTextLabel = %RichTextLabel
+@onready var next_button: Button = %NextButton   
+
+var dialogue_items: Array[String] = [ 
+	"Where am I?", 
+	"This place smells disgusting!", 
+	"I gotta find a way out of HERE.",
+] 
+
+var current_item_index := 0 
+
+func _ready() -> void: 
+	show_text() 
+	next_button.pressed.connect(advance)  
+	
+func show_text() -> void: 
+	var current_item := dialogue_items[current_item_index] 
+	rich_text_label.text = current_item  
+	
+func advance() -> void: 
+	current_item_index += 1 
+	
+	if current_item_index == dialogue_items.size(): 
+		get_tree().change_scene_to_file("res://world.tscn")
+		
+
+
+	else: 
+		show_text()
