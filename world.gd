d08a7d1661ac59318bcd47e6d5488ec20e20eac4
JordanTatum1 <159198043+JordanTatum1@users.noreply.github.com>
2025-04-10 22:44:32 -0700

    bomb timer stuff

--- world.gd
+++ world.gd
@@ -2,6 +2,20 @@ extends Node2D
 
 @onready var collision_polygon_2d = $StaticBody2D/CollisionPolygon2D
 @onready var polygon_2d = $StaticBody2D/CollisionPolygon2D/Polygon2D
+<<<<<<< Updated upstream
+=======
+@onready var level_completed = $CanvasLayer/LevelCompleted
+
+
+
+
+func _ready(): 
+	polygon_2d.polygon = collision_polygon_2d.polygon 
+	polygon_2d.polygon = collision_polygon_2d.polygon
+	Events.level_completed.connect(show_level_completed)
+	
+
+>>>>>>> Stashed changes
 
 func _ready(): 
 	polygon_2d.polygon = collision_polygon_2d.polygon
