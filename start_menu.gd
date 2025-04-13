4aee4cb749dd5fdcf441ec4f37654c2f00749123
JordanTatum1 <159198043+JordanTatum1@users.noreply.github.com>
2025-04-12 21:30:50 -0700

    level transition and bomb death

--- start_menu.gd
+++ start_menu.gd
@@ -1,8 +1,9 @@
 extends CenterContainer
 
 func _on_start_game_button_pressed() -> void:  
+	await LevelTransition.fade_to_black()
 	get_tree().change_scene_to_file("res://world.tscn")
-
+	LevelTransition.fade_from_black()
 
 func _on_quit_button_pressed() -> void:
 	get_tree().quit()
--- world.gd
+++ world.gd
@@ -1,8 +1,13 @@
-extends Node2D
+extends Node
+
+@export var next_level : PackedScene
+
 
 @onready var collision_polygon_2d = $StaticBody2D/CollisionPolygon2D
 @onready var polygon_2d = $StaticBody2D/CollisionPolygon2D/Polygon2D
 @onready var level_completed = $CanvasLayer/LevelCompleted
+
+
 func _ready(): 
 	polygon_2d.polygon = collision_polygon_2d.polygon 
 	polygon_2d.polygon = collision_polygon_2d.polygon
@@ -11,3 +16,8 @@ func _ready():
 func show_level_completed():
 	level_completed.show()
 	get_tree().paused = true
+	await LevelTransition.fade_to_black()
+	get_tree().paused = false
+	if not next_level is PackedScene : return
+	get_tree().change_scene_to_packed(next_level)
+	LevelTransition.fade_from_black()
