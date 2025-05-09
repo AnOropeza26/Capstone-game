6c29fcf8a20ff9ccbb45a6ddc541f7f523c7da6d
JordanTatum1 <159198043+JordanTatum1@users.noreply.github.com>
2025-05-09 12:03:14 -0700

    new

--- timer_reset.gd
+++ timer_reset.gd
@@ -1,7 +1,20 @@
 extends Area2D
 
+<<<<<<< Updated upstream
 
 
 func _on_body_entered(body: Node2D) -> void:
 	pass # Replace with function body.
 	queue_free()
+=======
+@onready var my_timer: Timer = get_node("res://timer.tscn")
+
+
+func _on_body_entered(body):
+	if body.name == "Player":
+		var timer = get_node("res://timer.tscn")
+		if timer:
+			timer.stop()
+			timer.start()
+		queue_free() 
+>>>>>>> Stashed changes
